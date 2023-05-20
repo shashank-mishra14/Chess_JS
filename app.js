9cc812bdf4cbabaa4ae2103b8f1550b7aaef5b8f
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-05-21 05:08:30 +0530

    creating game board

--- app.js
+++ app.js
@@ -4,4 +4,26 @@ const infoDisplay = document.querySelector("#info-display");
 
 const width = 8;
  
-const startPieces = [];
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
+];
+
+function createBoard(){
+  startPieces.forEach((startPiece)=>{
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.classList.add('biege');
+    gameBoard.append(square);
+
+    });
+}
+  
+
+    
